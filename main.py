4aaf9ee775f89acb22c4007e8ee02881af94cfa5
MJ-Dr <s235551@dtu.dk>
2024-09-23 15:07:28 +0200

    test

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ doorResult = doorRule.checkRule(model)
 print("Window result:", windowResult)
 print("Door result:", doorResult)
 
-import ifcopenshell
-from bonsai.bim.ifc import IfcStore
-file = IfcStore.get_file()
-things = file.by_type('IfcPlate')
-print(len(things))
+
 
 
