21f14efe34c8e710c1345dd79e3154439d83c7b9
MJ-Dr <s235551@dtu.dk>
2024-09-23 15:48:44 +0200

    test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import ifcopenshell
 
-
 from rules import windowRule
 
 model = ifcopenshell.open("C:\\Users\\mikae\\OneDrive\\Dokumenter\\DTU BYGNINGSDESIGN\\3.semster\\41934 Advanced Building Information Modeling\\CES_BLD_24_06_ARC.IFC")
--- rules/windowRule.py
+++ rules/windowRule.py
@@ -7,3 +7,4 @@ def checkRule(model):
 
     return result
 
+
